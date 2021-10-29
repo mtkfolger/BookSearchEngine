d2bfb433296830e29f437b1a6f6c9577b037d64c
Michael Folger <folger@zoller-usa.com>
2021-10-29 15:29:33 -0500

    fixed typos in server.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const path = require('path');
 
+//import apollo server
 const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-
+//db connection
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
+//apply apollo server with express ap
 server.applyMiddleware({app});
 
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -30,14 +36,16 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-//get everything
-app.get('*', (req,res) => {
+//get all
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 db.once('open', () => {
   app.listen(PORT, () => {
-  console.log(`🌍 Now listening on localhost:${PORT}`)
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-});
-});
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}); 
+
+
