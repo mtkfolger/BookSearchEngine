b56f5b2007425c489b40438fc8db2ad42be4ccf7
Michael Folger <folger@zoller-usa.com>
2021-10-29 14:48:59 -0500

    updated server.js to include correct routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
+
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +30,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+//get everything
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`)
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
 });
